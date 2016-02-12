96454262eb935938c5b5bda8af75cf25c35e4a10
Aditya Ghan <aditya.ghan@blisstering.com>
2016-02-12 11:36:49 +0530

    Minor enhancements

--- src/app/samplecomponent/samplecomponent.ts
+++ src/app/samplecomponent/samplecomponent.ts
@@ -3,12 +3,12 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'sample-app',
     template: `<div class="sample">
-        <h1> Hello world, this is {{name}} </h1>
+        <h1> Hello world, this is {{ name }} </h1>
     </div>`
 })
 export class SampleComponent {
     name: string;
     constructor() {
-        this.name = 'Aditya';
+        this.name = 'Angular2 !';
     }
 }
